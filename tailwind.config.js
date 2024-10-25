d980c7353884a31f7af7962f57c8acbc0d7ecb4a
lyb9030 <lyb_9030@naver.com>
2024-10-25 09:19:38 +0900

    테마 환경구성

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "1600px",
+      },
+      width: {
+        100: "25rem",
+        128: "32rem",
+      },
+      fontSize: {
+        "10xl": [
+          "10rem",
+          {
+            lineHeight: 1,
+            letterSpacing: "-0.02em",
+          },
+        ],
+      },
+    },
   },
   plugins: [],
 };
